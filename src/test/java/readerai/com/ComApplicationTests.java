d09fae984cfe4ddfb8702f3e12bb2828c35a969c
VlaTal1 <vladiktalah@gmail.com>
2025-03-08 11:41:02 +0200

    init

---
+++ src/test/java/readerai/com/ComApplicationTests.java
@@ -0,0 +1,13 @@
+package readerai.com;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
